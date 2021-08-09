8e36776354a754ce069e1bb3cd231505bc0fc2bd
David01051 <76750253+David01051@users.noreply.github.com>
2021-08-09 17:05:12 +0300

    add .env

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8080 ;
+const CONNECTION_URL= process.env.CONNECTION_URL ;
+const studentRoute = require('./src/routes/studentsRouter')
+const db = require('./src/DB') // index יתפוס קבצי
+const express = require('express');
+const cors = require("cors");
+const app = express();
+
+
+app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
+app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
+app.use(cors());
+
+db.on('error',()=> console.log("error"))
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up one port:${PORT}`);
+})
+
+app.use('/school',studentRoute)
+
+app.get('/', (req,res)=>{
+    res.send("Hello World")
+})
